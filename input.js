6815c62cfd8ae23bfe3893df6a39c71eb6e10838
Dylan Palin <dylanpalin@gmail.com>
2023-11-23 13:55:57 -0700

    passed conn into stdin

--- input.js
+++ input.js
@@ -1,21 +1,22 @@
 const net = require("net");
+let connection;
 
 const handleUserInput = function (key) {
-  // if (key === '\u0003') {
-  //   process.exit();
-  // } else if (key === 'w') {
-  //   connection.write("Move: up");
-  // } else if (key === 'a') {
-  //   connection.write("Move: left");
-  // } else if (key === 's') {
-  //   connection.write("Move: down");
-  // } else if (key === 'd') {
-  //   connection.write("Move: right");
-  // }
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  }
 }
 
-const setupInput = function () {
-  conn = connect();
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -2,8 +2,10 @@ const net = require("net");
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
+const conn = connect();
+
+setupInput(conn);
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+
