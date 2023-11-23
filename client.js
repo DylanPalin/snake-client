8a8ef378488723c4c788804e139cf56154d2b8b3
Dylan Palin <dylanpalin@gmail.com>
2023-11-23 10:55:55 -0700

    exported connect function

--- client.js
+++ client.js
@@ -1,25 +1,21 @@
-// const net = require("net");
+const net = require("net");
 
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "165.227.47.243",
-//     port: 50541,
-//   });
-  
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
 
-//     conn.on("data", (data) => {
-//       console.log(data);
-//     });  
+  conn.on('data', (data) => {
+    console.log(data);
+  });
 
-//     conn.on("connect", () => {
-//       console.log("Successfully connected to game server");
-//         conn.write("Name: DYL");
-//     });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
-//   return conn;
+  return conn;
+};
 
-// };
 
-// module.exports = { connect };
+module.exports = { connect };
