bfa3a08a0e3d66f9617ca7df358aeb2f91b65383
Dylan Palin <dylanpalin@gmail.com>
2023-11-23 10:52:28 -0700

    moved assignment files to proper folder

---
+++ client.js
@@ -0,0 +1,25 @@
+// const net = require("net");
+
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541,
+//   });
+  
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+
+//     conn.on("data", (data) => {
+//       console.log(data);
+//     });  
+
+//     conn.on("connect", () => {
+//       console.log("Successfully connected to game server");
+//         conn.write("Name: DYL");
+//     });
+
+//   return conn;
+
+// };
+
+// module.exports = { connect };
